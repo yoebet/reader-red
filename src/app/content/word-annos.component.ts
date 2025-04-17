9dd22b026f7ffedc3d83380d6438b9fe2ffe599a
hawks <yobett@hxr.com>
2025-04-17 13:57:22 +0800

    word-annos

--- src/app/content/word-annos.component.ts
+++ src/app/content/word-annos.component.ts
@@ -1,9 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 
-import {UIConstants} from '../config';
 import {AnnotationSet} from '../anno/annotation-set';
-import {DictEntry} from '../models/dict-entry';
-import {DictService} from '../services/dict.service';
+import {AnnotatorHelper} from '../anno/annotator-helper';
+import {ElAnnos} from '../anno/el-annos';
 
 @Component({
   selector: 'word-annos',
@@ -11,141 +10,43 @@ import {DictService} from '../services/dict.service';
   styleUrls: ['./word-annos.component.css']
 })
 export class WordAnnosComponent implements OnInit {
-  @Input() _wordEl: Element;
-  @Input() paraTextEl: Element;
+  // tslint:disable-next-line:variable-name
+  @Input() _wordEl: HTMLElement;
+  @Input() paraTextEl: HTMLElement;
   @Input() enabled: boolean;
   @Input() annotationSet: AnnotationSet;
-  word: string;
+
   head: string;
-  items: any[];
-  note: string;
-  meaning: any;
+  annos: ElAnnos;
   private initialized = false;
 
-  constructor(private dictService: DictService) {
-  }
 
   ngOnInit() {
     this.initialized = true;
   }
 
-  set wordEl(_wordEl: Element) {
-    if (_wordEl === this._wordEl) {
-      return;
-    }
-    this._wordEl = _wordEl;
-    if (this.initialized && this.enabled) {
-      this.parseAnnotations();
+  set wordEl(wordEl0: HTMLElement) {
+    this._wordEl = wordEl0;
+    if (this.initialized && this.enabled && this.annotationSet) {
+      this.annos = AnnotatorHelper.parseAnnotations(this._wordEl, this.annotationSet, this.paraTextEl);
+
+      let annos = this.annos;
+      this.head = annos.word;
+      if (!annos.note && annos.items.length === 0) {
+        if (!annos.meaning && annos.phraseMeaning) {
+          this.head = annos.phraseMeaning.word;
+        } else if (annos.meaning && !annos.phraseMeaning) {
+          this.head = annos.meaning.word;
+        }
+      }
+      if (this.head.length > 20) {
+        this.head = this.head.substring(0, 20) + '...';
+      }
     }
   }
 
-  get wordEl(): Element {
+  get wordEl(): HTMLElement {
     return this._wordEl;
-  }
-
-  private parseAnnotations() {
-    this.items = [];
-    this.note = null;
-    this.meaning = null;
-    if (!this._wordEl) {
-      this.word = null;
-      this.head = null;
-      return;
-    }
-    this.word = this._wordEl.textContent;
-    this.head = this.word;
-    let phraseGroup = null;
-
-    let attributes = Array.from(this._wordEl.attributes);
-    for (let {name, value} of attributes) {
-      if (!name.startsWith('data-')) {
-        continue;
-      }
-      name = name.substr(5);
-      if (name === 'mid') {
-        let mid = parseInt(value);
-        if (isNaN(mid)) {
-          continue;
-        }
-        let forWord = this._wordEl.getAttribute('data-word');
-        if (!forWord) {
-          forWord = this.word;
-        }
-        this.meaning = {mid, word: forWord, text: ''};
-
-        let theWordEl = this._wordEl;
-        this.dictService.getEntry(forWord, {complete: true})
-          .subscribe((entry: DictEntry) => {
-              if (!entry) {
-                return;
-              }
-              if (this._wordEl !== theWordEl) {
-                return;
-              }
-              if (!entry.complete) {
-                return;
-              }
-              for (let posMeaning of entry.complete) {
-                let items = posMeaning.items;
-                for (let item of items) {
-                  if (item.id === mid) {
-                    let meaningText = `${posMeaning.pos}${item.exp}`;
-                    this.meaning = {mid, word: forWord, text: meaningText};
-                  }
-                }
-              }
-            }
-          );
-        continue;
-      }
-      if (name === 'note') {
-        this.note = value;
-        continue;
-      }
-      if (name === 'phra' && /^g\d$/.test(value)) {
-        phraseGroup = value;
-        continue;
-      }
-      if (!this.annotationSet) {
-        continue;
-      }
-      let text = this.annotationSet.annotationOutput(name, value);
-      if (!text) {
-        continue;
-      }
-      let item = {dataName: name, dataValue: value, text};
-      this.items.push(item);
-    }
-
-    if (phraseGroup && this.items.length === 0 && !this.note && !this.meaning) {
-      let stEl = this.findSentence(this.wordEl);
-      if (!stEl) {
-        stEl = this.paraTextEl;
-      }
-      let groupSelector = `[data-phra=${phraseGroup}]`;
-      let groupEls = stEl.querySelectorAll(groupSelector);
-      let els = Array.from(groupEls);
-      this.head = els.map((el: Element) => el.textContent).join(' ');
-    }
-    if (this.head.length > 20) {
-      this.head = this.head.substring(0, 20) + '...';
-    }
-  }
-
-  private findSentence(node): any {
-    do {
-      if (node instanceof Element) {
-        let el = node as Element;
-        if (el === this.paraTextEl) {
-          return null;
-        }
-        if (el.matches(UIConstants.sentenceTagName)) {
-          return el;
-        }
-      }
-      node = node.parentNode;
-    } while (node);
-    return null;
   }
 
 }
