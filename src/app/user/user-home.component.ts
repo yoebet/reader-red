0050ba2845287845cefd49d3d7a8fe3ef8637c7f
hawks <yobett@hxr.com>
2025-05-25 23:54:21 +0800

    menu

--- src/app/user/user-home.component.ts
+++ src/app/user/user-home.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user-home',
   templateUrl: './user-home.component.html',
+  styleUrls: ['./user-home.component.css']
 })
 export class UserHomeComponent {
 }
